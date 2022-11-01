b031902457c147266652519e958921380f4689ff
issueWanatabeSho <78394259+wtnbSho@users.noreply.github.com>
2022-11-01 16:40:54 +0900

    MainTodo追加

---
+++ src/components/MainTodo.vue
@@ -0,0 +1,33 @@
+<script setup>
+</script>
+
+<template>
+	<div class="box_input">
+		<input type="text" class="todo_input" placeholder="+ TODOを入力" />
+		<button class="btn">追加</button>
+	</div>
+</template>
+
+<style scoped>
+.box_input {
+	margin-top: 20px;
+}
+
+.todo_input {
+	width: 300px;
+	margin-right: 8px;
+	padding: 8px;
+	font-size: 18px;
+	border: 1px solid #aaa;
+	border-radius: 6px;
+}
+
+.btn {
+	padding: 8px;
+	background-color: #03a9f4;
+	border-radius: 6px;
+	color: #fff;
+	text-align: center;
+	font-size: 14px;
+}
+</style>
